b18518778a8effb0245fe8d744987329fd219d88
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-06-09 12:54:46 -0300

    feat: setting App widget.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ddm_av05/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CepToMapApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
